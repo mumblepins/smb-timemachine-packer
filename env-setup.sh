fa1e6cc76965ba8e1d1a101c7f7dbfbbb3799854
Daniel Sullivan <dansull@gmail.com>
2018-02-16 15:34:55 -0600

    user

--- env-setup.sh
+++ env-setup.sh
@@ -3,7 +3,7 @@ set -ex
 add-apt-repository -y ppa:ubuntu-lxc/stable
 apt-get -q update
 apt-get install -y lxd
-usermod -a -G lxd $(whoami)
+usermod -a -G lxd travis
 wget https://releases.hashicorp.com/packer/1.2.0/packer_1.2.0_linux_amd64.zip -O /tmp/packer.zip
 cd /tmp && unzip packer.zip
 mv /tmp/packer /usr/local/bin/packer
