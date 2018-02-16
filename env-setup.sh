d262d92ff9b091164aecf63af28e1d871499ba98
Daniel Sullivan <dansull@gmail.com>
2018-02-16 15:52:19 -0600

    snap1

--- env-setup.sh
+++ env-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -ex
 apt-get -q update
+groupadd --system lxd
+usermod -a -G lxd travis
 apt-get install -y snapd
 snap install lxd
-usermod -a -G lxd travis
+
 wget https://releases.hashicorp.com/packer/1.2.0/packer_1.2.0_linux_amd64.zip -O /tmp/packer.zip
 cd /tmp && unzip packer.zip
 mv /tmp/packer /usr/local/bin/packer
