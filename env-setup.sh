2ce79913fa0aa4c64d811dad8ec11092c188395f
Daniel Sullivan <dansull@gmail.com>
2018-02-16 23:08:36 -0600

    env

--- env-setup.sh
+++ env-setup.sh
@@ -4,6 +4,7 @@ export PATH=/snap/bin:$PATH
 apt-get -q update
 groupadd --system lxd
 usermod -a -G lxd travis
+
 apt-get install -y snapd
 snap install lxd
 
@@ -14,3 +15,6 @@ chmod 755 /usr/local/bin/packer
 chown root:root /usr/local/bin/packer
 hash -r
 systemctl restart snap.lxd.daemon || true
+sleep 15
+set +e
+systemctl status snap.lxd.daemon
