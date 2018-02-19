caca1ed3d4863c2d16cb86b84c1fbd34f26abf52
Daniel Sullivan <dansull@gmail.com>
2018-02-18 20:15:42 -0600

    deps

--- cleanup.sh
+++ cleanup.sh
@@ -5,5 +5,7 @@ set -xe
 
 rm -rf --one-file-system /tmp/samba*
 apt-get autoremove --purge -y $BUILD_DEPS build-essential
+apt-get install -y libavahi-client3 libcups2 libjansson4
+
 rm -rf /var/lib/apt/lists/*
 rm -f /tmp/vars.sh
